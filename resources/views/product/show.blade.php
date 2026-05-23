bc889d10e884c577d6bdcc3298982f109eb2ca52
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2026-05-23 03:17:56 +0000

    firt commit 13

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -92,7 +92,9 @@ $productSchema = [
                 </span>
                 
                 <!-- Nom -->
-                <h1 class="text-2xl  mt-4">{{ $product->nom }}</h1>
+                <h1 class="text-2xl  mt-4">{{ $product->nom }} <svg class="w-4 h-4 inline-block mr-1" fill="currentColor" viewBox="0 0 20 20" style="color: #3b82f6;">
+    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+</svg></h1>
                 
                 <!-- Prix -->
                 <div class="mt-4">
