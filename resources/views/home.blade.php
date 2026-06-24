8d7ff1e1a476f574456ea4ecbcf17b71f63bf4f3
Abdoul Ahad Gueye <abdoulahadgueye1999@gmail.com>
2026-06-24 12:14:07 +0000

    firt commit 46

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -215,7 +215,7 @@
     
    <div class="mt-8">
     <div class="mb-4 px-4 py-2 w-full" style="background-color: #D3D4D2;">
-        <h2 class="text-lg font-bold mb-2">Découvrez  notre collection : Mai</h2>
+        <h2 class="text-lg font-bold mb-2">Découvrez  notre collection : Juillet</h2>
         <div class="flex items-center justify-between">
             <div class="flex items-center space-x-1">
                 <span class="text-xs"></span>
